3624c30342380d5ff420efd8e651e1c68f2147ca
Yash Arora <63657713+arorayash905@users.noreply.github.com>
2021-07-25 00:20:02 +0530

    Delete faceRecognizer.py

--- faceRecognizer.py
+++
@@ -1,9 +0,0 @@
-import test
-import cv2
-
-img = cv2.imread('try.jpeg')
-image = test.fun(img)
-cv2.imwrite("Recognized5.jpg", image)
-print("Image is Saved.")
-cv2.imshow("Image", image)
-cv2.waitKey(0)
